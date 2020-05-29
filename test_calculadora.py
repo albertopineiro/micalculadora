c528e201a7c69b987ae1248858f85f3499a39a38
albertopineiro <albertopineiroalarcon@gmail.com>
2020-05-29 13:25:24 +0200

    Subida del fichero test_calculadora sin framework de pruebas

--- test_calculadora.py
+++ test_calculadora.py
@@ -14,5 +14,53 @@ def test_suma():
 		print("SUMA INCORRECTA------------------")
 
 
+def test_resta():
+	# arrange
+	valor1 = 9
+	valor2 = 4
+	resultadoEsperado = 5
+	# act
+	r = resta(valor1, valor2)
+	# assert
+	if r == resultadoEsperado:
+		print("RESTA CORRECTA")
+	else:
+		print("RESTA INCORRECTA------------------")
+
+
+
+def test_multiplicacion():
+	# arrange
+	valor1 = 6
+	valor2 = 3
+	resultadoEsperado = 18
+	# act
+	r = multiplicacion(valor1, valor2)
+	# assert
+	if r == resultadoEsperado:
+		print("MULTIPLICACION CORRECTA")
+	else:
+		print("MULTIPLICACION INCORRECTA------------------")
+
+
+def test_division():
+	# arrange
+	valor1 = 27
+	valor2 = 3
+	resultadoEsperado = 9
+	# act
+	r = division(valor1, valor2)
+	# assert
+	if r == resultadoEsperado:
+		print("MULTIPLICACION CORRECTA")
+	else:
+		print("MULTIPLICACION INCORRECTA------------------")
+
+
+
+
 if __name__ == "__main__":
 	test_suma()
+	test_resta()
+	test_multiplicacion()
+	test_division()
