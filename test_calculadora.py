fc0cb11678d0aa7e3b8454da5c90f01ed87a7b0a
albertopineiro <albertopineiroalarcon@gmail.com>
2020-05-29 15:17:12 +0200

    Subida de test con framework de UNITTEST correcto

--- test_calculadora.py
+++ test_calculadora.py
@@ -1,79 +1,59 @@
+import unittest
 from calculadora import suma, resta, multiplicacion, division, raizcuadrada
 
-def test_suma():
-	# arrange
-	valor1 = 5
-	valor2 = 7
-	resultadoEsperado = 12
-	# act
-	r = suma(valor1, valor2)
-	# assert
-	if r == resultadoEsperado:
-		print("SUMA CORRECTA")
-	else:
-		print("SUMA INCORRECTA------------------")
+class TestCalculadora(unittest.TestCase):		
+
+	def test_suma(self):
+		# arrange
+		valor1 = 5
+		valor2 = 7
+		resultadoEsperado = 12
+		# act
+		resultado = suma(valor1, valor2)
+		# assert
+		self.assertEqual(resultado, resultadoEsperado)
 
 
-def test_resta():
-	# arrange
-	valor1 = 9
-	valor2 = 4
-	resultadoEsperado = 5
-	# act
-	r = resta(valor1, valor2)
-	# assert
-	if r == resultadoEsperado:
-		print("RESTA CORRECTA")
-	else:
-		print("RESTA INCORRECTA------------------")
+	def test_resta(self):
+		# arrange
+		valor1 = 9
+		valor2 = 4
+		resultadoEsperado = 5
+		# act
+		resultado = resta(valor1, valor2)
+		# assert
+		self.assertEqual(resultado, resultadoEsperado)
 
 
+	def test_multiplicacion(self):
+		# arrange
+		valor1 = 6
+		valor2 = 3
+		resultadoEsperado = 18
+		# act
+		resultado = multiplicacion(valor1, valor2)
+		# assert
+		self.assertEqual(resultado, resultadoEsperado)
 
-def test_multiplicacion():
-	# arrange
-	valor1 = 6
-	valor2 = 3
-	resultadoEsperado = 18
-	# act
-	r = multiplicacion(valor1, valor2)
-	# assert
-	if r == resultadoEsperado:
-		print("MULTIPLICACION CORRECTA")
-	else:
-		print("MULTIPLICACION INCORRECTA------------------")
-
-
-def test_division():
-	# arrange
-	valor1 = 27
-	valor2 = 3
-	resultadoEsperado = 9
-	# act
-	r = division(valor1, valor2)
-	# assert
-	if r == resultadoEsperado:
-		print("MULTIPLICACION CORRECTA")
-	else:
-		print("MULTIPLICACION INCORRECTA------------------")
+	def test_division(self):
+		# arrange
+		valor1 = 27
+		valor2 = 3
+		resultadoEsperado = 9
+		# act
+		resultado = division(valor1, valor2)
+		# assert
+		self.assertEqual(resultado, resultadoEsperado)
 
 
-def test_raiz():
-	# arrange
-	valor1 = 50
-	resultadoEsperado = 7.0710678
-	# act
-	r=raizcuadrada(valor1)
-	# assert
-	print(r)
-	if  abs(r-resultadoEsperado) < 0.0001:
-		print("RAIZ CORRECTA")
-	else:
-		print("RAIZ INCORRECTA------------------")
-
+	def test_raiz(self):
+		# arrange
+		valor1 = 50
+		resultadoEsperado = 7.071067811865475
+		# act
+		resultado=raizcuadrada(valor1)
+		# assert
+		self.assertLess(abs(resultado-resultadoEsperado), 0.00000001)
 
 if __name__ == "__main__":
-	test_suma()
-	test_resta()
-	test_multiplicacion()
-	test_division()
-	test_raiz()
+	unittest.main()
